0df76808fbe9e71e870ca3f9d19fd8399197ed83
tushar-savaliya <savaliyatushar2197@gmail.com>
2022-06-02 11:04:23 +0530

    changes in code

--- UserManagement/src/mongoServices/index.js
+++ UserManagement/src/mongoServices/index.js
@@ -1,4 +1,5 @@
-
 export { default as permissionService } from './adminManagement/permissionService';
 export { default as roleService } from './adminManagement/roleService';
 export { default as adminUserService } from './adminManagement/userService';
+export { default as drService } from './drService';
+export { default as CustomerService } from './customerService';
